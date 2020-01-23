5b984aa786782bd621c2203bff9f1fdf7c732428
James Singleton <james.singleton1@aexp.com>
2020-01-23 13:15:55 -0700

    chore(lockfile-lint): add lockfile-lint script

---
+++ lockFileLint.js
@@ -0,0 +1,38 @@
+const { spawnSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const invalidations = [];
+const defaultLockfilePath = `${process.cwd()}/package-lock.json`;
+const packagesBasePath = path.join(process.cwd(), 'packages');
+
+const pathsToValidate = [defaultLockfilePath].concat(
+  fs
+    .readdirSync(packagesBasePath)
+    .map(pathName => `${packagesBasePath}/${pathName}`)
+    .filter(pathName => fs.statSync(pathName).isDirectory())
+    .map(pathName => `${pathName}/package-lock.json`)
+);
+
+pathsToValidate.forEach(lockPath => {
+  const { stderr } = spawnSync('./node_modules/.bin/lockfile-lint', [
+    '-p',
+    lockPath,
+    '-t',
+    'npm',
+    '-a',
+    'npm',
+    '-o',
+    'https:',
+    '-c',
+    '-i',
+  ]);
+  const error = stderr.toString();
+  if (error) invalidations.push([lockPath, error].join(':\n\n'));
+});
+
+if (invalidations.length > 0) {
+  process.stderr.write(invalidations.join('\n'));
+  // eslint-disable-next-line unicorn/no-process-exit
+  process.exit(1);
+}
