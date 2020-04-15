d0acbcefb42aabc618e9142aae54ed04819cec23
Jonathan Golden <jonathan.ormsby@aexp.com>
2020-04-15 14:42:12 -0700

    chore(lerna): lerna deploy scripts

---
+++ lernaDeploy.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2019 American Express Travel Related Services Company, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express
+ * or implied. See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+const { exec } = require('child_process');
+// This regex was obtained from https://semver.org/ test it out here https://regex101.com/r/vkijKf/1/
+const regex = /^chore\(release\): (0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
+const commitMessage = process.argv[2]; // This is the commit message
+if (commitMessage != null && regex.test(commitMessage)) {
+  exec('npm run lerna:publish').stderr.pipe(process.stderr);
+}
