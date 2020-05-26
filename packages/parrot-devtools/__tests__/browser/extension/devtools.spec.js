cabda2e71d3ed7409106e7204566b2767079c8bc
James Coates <james.coates@novastonemedia.com>
2020-05-26 21:40:00 +0100

    fix(parrot-devtools) use firefox compatible paths for assets

--- packages/parrot-devtools/__tests__/browser/extension/devtools.spec.js
+++ packages/parrot-devtools/__tests__/browser/extension/devtools.spec.js
@@ -26,8 +26,8 @@ describe('devtools', () => {
   it('should setup tab', () => {
     expect(global.chrome.devtools.panels.create).toHaveBeenCalledWith(
       'Parrot',
-      'assets/img/parrot_48x.png',
-      'views/devtool-panel.html'
+      '/assets/img/parrot_48x.png',
+      '/views/devtool-panel.html'
     );
   });
 });
